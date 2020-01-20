4181bf1fdc3a5485a7951fa005a6f1e03e0b7e99
Entato <kaichengxu12@hotmail.com>
2020-01-20 18:02:53 -0500

    removed aaaa

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,7 +10,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-aaaaaaa
 
 /**
  * The VM is configured to automatically run this class, and to call the
