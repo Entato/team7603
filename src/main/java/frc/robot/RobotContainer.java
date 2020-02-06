e43219c6ebf9c0c89cf729ffae21a1e49c72566d
Kai Xu <48327213+Entato@users.noreply.github.com>
2020-02-05 23:59:20 -0500

    cleaned a few lines and added comments explaining how this thing works

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,8 +9,6 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 //this file creates the command and subsystem objects then binds them to a button on the controller
 //configureButtonBindings() method creates joystickbutton objects to call a command
 public class RobotContainer {
-  //initialise every subystem and command objects at the top
-
   //controller
   private final Joystick controller = new Joystick(Constants.joystickPort);
 
@@ -30,18 +28,17 @@ public class RobotContainer {
   //method for mapping buttons to call commands
   private void configureButtonBindings() {
 
+    //example button and command
     //create joystickbutton object with parameters of the joystick and the button ID
     JoystickButton exampleButton = new JoystickButton(controller, 1);
     //use joystickbutton, whenHeld(), whenPressed(), and whenReleased() methods to add commands
     exampleButton.whenHeld(m_autoCommand);
 
-    //left drive
+    //drives
     JoystickButton lAxis = new JoystickButton(controller, Constants.axisLY);
-    lAxis.whenHeld(driveCommand);
-
-    //right drive
     JoystickButton rAxis = new JoystickButton(controller, Constants.axisRY);
-    rAxis.whenHeld(driveCommand);
+    //or statement to check if either the lAxis or rAxis is active
+    rAxis.or(lAxis).whenActive(driveCommand);
   }
 
   //used for calling the autonomous command
--- src/main/java/frc/robot/subsystems/DriveChain.java
+++ src/main/java/frc/robot/subsystems/DriveChain.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Victor;
@@ -27,12 +20,12 @@ public class DriveChain extends SubsystemBase {
 
   //sets the left drive of the robot
   public void driveLeft(double speed){
-    LDrive.set(speed);
+    LDrive.set(speed * Constants.driveLimit);
   }
 
   //sets the right drive of the robot
   public void driveRight(double speed){
-    RDrive.set(speed);
+    RDrive.set(speed * Constants.driveLimit);
   }
 
   public void stop(){
