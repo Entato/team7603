dad2a03aec4b873913a3eefaac6b479a483a72d8
Victor Yu <victorthelord123@gmail.com>
2020-02-14 00:36:46 -0500

    Look Kai I did something Aren't you proud?

--- src/main/java/frc/robot/commands/ClimbCommand.java
+++ src/main/java/frc/robot/commands/ClimbCommand.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class ClimbCommand extends CommandBase {
   @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
   private final Climber arm;
+
   /**
    * Creates a new ExampleCommand.
    *
@@ -26,7 +27,7 @@ public class ClimbCommand extends CommandBase {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(arm);
   }
-  
+
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
