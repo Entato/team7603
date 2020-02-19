c8d130c7d102b19f7e48d1f294de1a1cd979542a
Entato <kaichengxu12@hotmail.com>
2020-02-19 16:37:19 -0500

    small changes

---
+++ src/main/java/frc/robot/commands/ManualLift.java
@@ -0,0 +1,5 @@
+package frc.robot.commands;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.Climber;
+import edu.wpi.first.wpilibj2.command.CommandBase;
--- src/main/java/frc/robot/commands/ManualSpinner.java
+++ src/main/java/frc/robot/commands/ManualSpinner.java
@@ -1,5 +1,5 @@
 package frc.robot.commands;
 
 import frc.robot.Constants;
-import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Spinner;
 import edu.wpi.first.wpilibj2.command.CommandBase;
