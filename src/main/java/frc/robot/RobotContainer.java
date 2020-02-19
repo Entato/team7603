ed207e87a29e556a4b7352915a0f94bf6c8bd32b
unknown <kaichengxu12@hotmail.com>
2020-02-18 17:34:09 -0800

    added commands to RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,17 +13,21 @@ public class RobotContainer {
   private final Joystick controller = new Joystick(Constants.joystickPort);
 
   //Subsystems
-  private final Spinner m_shooter = new Spinner();
+  private final Spinner spinner = new Spinner();
   private final DriveChain driveChain = new DriveChain();
   private final Shooter shooter = new Shooter();
   private final Intake intake = new Intake();
   private final Conveyer conveyer = new Conveyer();
+  private final Climber climber = new Climber();
 
   //Commands
-  private final SpinnerCommand m_colorSensor = new SpinnerCommand(m_shooter);
+  private final SpinnerCommand colorSensorCommand = new SpinnerCommand(spinner);
   private final DriveCommand driveCommand = new DriveCommand(driveChain, controller);
   private final ShootCommand shootCommand = new ShootCommand(shooter);
   private final IntakeCommand intakeCommand = new IntakeCommand(intake, conveyer);
+  private final ClimbCommand climbCommand = new ClimbCommand(climber);
+  private final LiftCommand liftCommand = new LiftCommand(climber);
+
 
   //ignore this for now
   public RobotContainer() {
@@ -42,13 +46,21 @@ public class RobotContainer {
     JoystickButton shootButton = new JoystickButton(controller, Constants.buttonA);
     shootButton.whenHeld(shootCommand);
     
-    JoystickButton aButton = new JoystickButton(controller, 1);
-    aButton.whenHeld(m_colorSensor);
+    //sensor
+    JoystickButton colorSensorButton = new JoystickButton(controller, Constants.buttonX);
+    colorSensorButton.whenHeld(colorSensorCommand);
 
     //intake
-    JoystickButton bButton = new JoystickButton(controller, Constants.buttonB);
-    bButton.whenHeld(intakeCommand);
+    JoystickButton intakeButton = new JoystickButton(controller, Constants.buttonB);
+    intakeButton.whenHeld(intakeCommand);
 
+    //climber
+    JoystickButton climbButton = new JoystickButton(controller, Constants.buttonY);
+    climbButton.whenHeld(climbCommand);
+
+    //lift
+    JoystickButton liftButton = new JoystickButton(Controller, Constants.buttonLB);
+    liftButton.whenHeld(liftCommand);
   }
 
   //used for calling the autonomous command
