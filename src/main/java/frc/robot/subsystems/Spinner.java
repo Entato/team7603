6cf8a213356003a96499b0a3568a7f02c9c1c9fe
unknown <kaichengxu12@hotmail.com>
2020-02-18 17:48:36 -0800

    changed spinner subsystem

--- src/main/java/frc/robot/subsystems/Spinner.java
+++ src/main/java/frc/robot/subsystems/Spinner.java
@@ -8,12 +8,15 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.I2C;
 import com.revrobotics.ColorSensorV3;
 
@@ -21,91 +24,97 @@ public class Spinner extends SubsystemBase {
     private final I2C.Port i2cPort = I2C.Port.kOnboard;
     private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
     private final Victor spinner = new Victor(Constants.spinner);
-    private final DoubleSolenoid solenoid = new DoubleSolenoid(0,1);
-
-    /**
-     * Creates a new ExampleSubsystem.
-     */
-    boolean nullChecker = true;
-    int switches = 0;
-    String startColor = "null";
-    String tempColor = "null";
-    String color = "null";
-    String spinning = "true";
+    private final DoubleSolenoid solenoid = new DoubleSolenoid(0, 1);
 
     public Spinner() {
     }
-    public void spin(){
+
+    public void spin() {
         spinner.set(1);
     }
-    public void stopSpin(){
+
+    public void stopSpin() {
         spinner.set(0);
     }
-    public void liftSpinnerArm() {
-        solenoid.set(Value.kForward);
+
+    public void upLift() {
+        solenoid.set(DoubleSolenoid.Value.kForward);
     }
-    public void dropSpinnerArm() {
-        solenoid.set(Value.kReverse);
+
+    public void downLift() {
+        solenoid.set(DoubleSolenoid.Value.kReverse);
     }
-    public void sensor() {
-        spin();
+
+    // //Setter for target color
+    // public void setTargetColor(String targetColor){
+    // this.targetColor = targetColor;
+    // }
+    public String checkColor() {
         Color detectedColor = m_colorSensor.getColor();
         double IR = m_colorSensor.getIR();
         double red = (detectedColor.red * 3);
         double green = (detectedColor.green * 2);
         double blue = (detectedColor.blue * 4);
-        double speed = 0.5;
-        SmartDashboard.putNumber("Red", detectedColor.red);
-        SmartDashboard.putNumber("Green", detectedColor.green);
-        SmartDashboard.putNumber("Blue", detectedColor.blue);
+        // Displays Colors
+        SmartDashboard.putNumber("Red", red);
+        SmartDashboard.putNumber("Green", green);
+        SmartDashboard.putNumber("Blue", blue);
+        // Checks for Yellow
         if (red > 1.14 && red < 1.29 && blue > 0.27 && blue < 0.47 && green > 0.92 && green < 1.4) {
-            color = "Yellow";
-            if (nullChecker == true && tempColor != color) {
-                switches++;
-                tempColor = color;
+            return "Yellow";
+        }
+        // Checks for Red
+        else if (red > green && red > blue) {
+            return "Red";
+        }
+        // Checks for Green
+        else if (green > red && green > blue) {
+            return "Green";
+        }
+        // Checks for Blue
+        else if (blue > red && blue > green) {
+            return "Blue";
+        }
+        return null;
+    }
+
+    // Method that does 1 full spinner revolution without stopping
+    public void spinOnce() {
+        int colorChanges = 0;
+        // Previous color (Initialized as starting color)
+        String oldColor = checkColor();
+        // Current color (currentColor being sensed)
+        String currentColor;
+
+        // One full revolution is 8 color changes
+        while (colorChanges < 8) {
+            spin();
+            currentColor = checkColor();
+            // Checks when color changes
+            if (!(currentColor.equals(oldColor))) {
+                // Adds to revolutions counter
+                colorChanges++;
+                // Changes current color to the "old color"
+                oldColor = currentColor;
             }
 
-        } else if (red > 1.14 && red < 1.29 && blue > 0.27 && blue < 0.47 && green > 0.92 && green < 1.4) {
-            color = "Red";
-            if (nullChecker == true && tempColor != color) {
-                switches++;
-                tempColor = color;
-            }
-        } else if (red > 1.14 && red < 1.29 && blue > 0.27 && blue < 0.47 && green > 0.92 && green < 1.4) {
-            color = "Green";
-            if (nullChecker == true && tempColor != color) {
-                switches++;
-                tempColor = color;
-            }
-        } else if (red > 1.14 && red < 1.29 && blue > 0.27 && blue < 0.47 && green > 0.92 && green < 1.4) {
-            color = "Blue";
-            if (nullChecker == true && tempColor != color) {
-                switches++;
-                tempColor = color;
-            }
         }
+    }
 
-        if (color != null && nullChecker == false) {
-            startColor = color;
-            nullChecker = true;
+    // Method that spins to the selected Color
+    public boolean goToColor(String color) {
+        String currentColor = checkColor();
+        if (currentColor != color) {
+            spin();
+            return goToColor(color);
         }
-
-        SmartDashboard.putString("Current_Color", color);
-        int proximity = m_colorSensor.getProximity();
-        SmartDashboard.putNumber("Switches", switches);
-        SmartDashboard.putString("Start_color", startColor);
-        SmartDashboard.putString("Spinning", spinning);
-        if (color == "Red") {
-            speed = (-0.4);// brake function
-            spinning = "False";
-            stopSpin();
-        }
+        stopSpin();
+        return true;
     }
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
     }
-
 
 }
