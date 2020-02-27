9245b6b75dade5ee1d418aca8cc837537cf3192e
unknown <kaichengxu12@hotmail.com>
2020-02-26 18:46:04 -0800

    cleaned code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,7 +2,7 @@ package frc.robot;
 
 public final class Constants {
     // Speed limits
-    public static double driveLimit = 0.8;
+    public static final double driveLimit = 0.8;
     public static final double shooterLimit = -0.5;
     public static final double spinnerLimit = 1;
     public static final double armLimit = 1;
@@ -47,35 +47,6 @@ public final class Constants {
     // sensor values
     public static final int limitSensor = 9;
 
-    // color values
-    public static final double redMinGreen = 0.425;
-    public static final double redMaxGreen = 0.438;
-    public static final double blueMinGreen = 0.120;
-    public static final double blueMaxGreen = 0.139;
-    public static final double greenMinGreen = 0.425;
-    public static final double greenMaxGreen = 0.438;
-
-    public static final double redMinYellow = 0.415;
-    public static final double redMaxYellow = 0.434;
-    public static final double blueMinYellow = 0.144;
-    public static final double blueMaxYellow = 0.164;
-    public static final double greenMinYellow = 0.415;
-    public static final double greenMaxYellow = 0.434;
-
-    public static final double redMinRed = 0.404;
-    public static final double redMaxRed = 0.422;
-    public static final double blueMinRed = 0.162;
-    public static final double blueMaxRed = 0.182;
-    public static final double greenMinRed = 0.404;
-    public static final double greenMaxRed = 0.422;
-
-    public static final double redMinBlue = 0.429;
-    public static final double redMaxBlue = 0.449;
-    public static final double blueMinBlue = 1.01;
-    public static final double blueMaxBlue = 1.210;
-    public static final double greenMinBlue = 0.429;
-    public static final double greenMaxBlue = 0.449;
-
     //Autonomous variables
     public static final long driveTime1 = 1000;
     public static final long pivotTime1 = 1500;
@@ -83,9 +54,6 @@ public final class Constants {
     public static final long pivotTime2 = 1500;
     public static final long endTime = 3000;
     public static double driveSpeed = 1;
-
-
-
     
     // time limits
     public static final double intakeConveyer = 350;
